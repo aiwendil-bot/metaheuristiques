39fe5e89f818217d97ed2e7d16877a9b3c20ef75
adrien <adriencallico@hotmail.fr>
2021-09-16 20:27:52 +0200

    récupération des data / code fourni / ouverture d'un fichier d'instance

--- DM1/main.jl
+++ DM1/main.jl
@@ -1,1 +1,9 @@
-print("hello world!")
+include("loadSPP.jl")
+include("getfname.jl")
+
+cd("$(homedir())/github/metaheuristiques")
+#test ouvrir un fichier d'instance
+fname = "DM1/Data/didactic.dat"
+cost, matrix = loadSPP(fname)
+println(cost)
+println(matrix)
