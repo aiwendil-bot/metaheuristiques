bb85c4aca81e11d10836595434b89dbf6d14c691
adrien <adriencallico@hotmail.fr>
2021-09-17 13:09:37 +0200

    heuristique v2 (x_i > 1)

--- DM1/main.jl
+++ DM1/main.jl
@@ -14,23 +14,37 @@ function verif_contrainte(vec1,vec2)
     return dot(vec1,vec2) < 1
 end
 
+function remove!(a, item)
+    deleteat!(a, findall(x->x==item, a))
+end
+
+
 #construction gloutonne d'une solution x_0 admissible et "de bonne facture"
-#ne fonctionne pas
 function greedy_construction(cost,matrix)
     dim_matrix = size(matrix)
     x_0 = zeros(Int, dim_matrix[2])
-
-    cost_sorted = unique(sort(cost,rev=true)) #on trie les poids par ordre décroissant et on supprime les doublons
-
-    #pour c
-    for i in cost_sorted
-        for j in 1:dim_matrix[1]
-            if matrix[j,i] == 1 && verif_contrainte(x_0,matrix[j,:])
-                x_0 = x_0 + matrix[j,:]
+    sous_ensembles_restants = 1:dim_matrix[2]
+    sous_ensembles_restants = convert(Vector,sous_ensembles_restants)
+    while length(sous_ensembles_restants) > 0
+        sous_ensemble_candidat= zeros(Int,dim_matrix[2])
+        max::Float16 = 0.0
+        indice_choisi::Int = 0
+        for compteur in sous_ensembles_restants
+            if verif_contrainte(sous_ensemble_candidat,x_0) && cost[compteur]/sum(matrix[compteur,:])>max
+                sous_ensemble_candidat = matrix[compteur,:]
+                max = cost[compteur]/sum(matrix[compteur,:])
+                indice_choisi = compteur
+            end
+        end
+        sous_ensembles_restants = remove!(sous_ensembles_restants,indice_choisi)
+        x_0 = x_0 + sous_ensemble_candidat
+        for compteur in sous_ensembles_restants
+            if dot(x_0,matrix[compteur,:])==1
+                remove!(sous_ensembles_restants,compteur)
             end
         end
     end
     return x_0
 end
-
+println(greedy_construction(cost,matrix))
 dot(cost,greedy_construction(cost,matrix))
