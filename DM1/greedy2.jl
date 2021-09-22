ae9bfe6a61bddadfbebd9280806e940bc10c410a
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2021-09-22 16:58:55 +0200

    greedy2 intermédiaire

--- DM1/greedy2.jl
+++ DM1/greedy2.jl
@@ -8,19 +8,33 @@ cost, matrix = loadSPP(fname)
 
 function greedy_construction(cost, matrix)
     copie_matrix = copy(matrix)
-    x0 = zeros(size(matrix)[1])
+    x0 = zeros(size(matrix)[2])
     subset_restant = ones(size(matrix)[1])
+    ratio = Vector{Float64}(undef, size(matrix)[2])
+    println(size(ratio))
     while sum(subset_restant) > 0
-        somme = sum(copie_matrix, dims=2)
-        ratio = cost / somme
+        somme = sum(copie_matrix, dims=1)
+        for i in 1:size(copie_matrix)[2]
+            if somme[i] != 0
+                ratio[i] = cost[i] / somme[i]
+            end
+        end
         indice_ratio_max = argmax(ratio)
         x0[indice_ratio_max] = 1
         subset_restant[indice_ratio_max] = 0
         for i in copie_matrix[indice_ratio_max, :]
             i = 0
         end
+        for i in 1:size(matrix)[2]
+            if dot(copie_matrix[i,:], x0) >= 1
+                subset_restant[i] = 0
+                for j in copie_matrix[i, :]
+                    j = 0
+                end
+            end
+        end
     end
     return x0, dot(x0, cost)
 end
 
-# greedy_construction(cost, matrix)
+greedy_construction(cost, matrix)
